a937584833d51724db7f0bd5a9824708d5e6ebd4
bdubyapee <phippsb@gmail.com>
2020-02-02 13:19:59 -0700

    Restructured to pass payload to coroutines instead of full message.

--- parse.py
+++ parse.py
@@ -39,49 +39,48 @@ async def softboot_game(wait_time: int) -> None:
     subprocess.Popen(["python3.8", "/home/bwp/PycharmProjects/akriosmud/src/akrios.py"])
 
 
-async def msg_heartbeat(msg: Dict[str, Dict[str, str]]) -> None:
-    log.debug(f"The full message is: {msg}")
+async def msg_heartbeat() -> None:
     log.debug(f"Heartbeat received from game at: {time.time()}")
 
 
-async def msg_players_output(msg: Dict[str, Dict[str, str]]) -> None:
+async def msg_players_output(payload: Dict[str, str]) -> None:
     """
         The msg is output for a player.  We .put that message into the asyncio.queue for that specific
         player.
     """
-    session = msg["payload"]["uuid"]
-    message = msg["payload"]["message"]
+    session = payload["uuid"]
+    message = payload["message"]
     if session in clients.connections:
         asyncio.create_task(messages_to_clients[session].put(Message("IO", message)))
 
 
-async def msg_players_sign_in(msg: Dict[str, Dict[str, str]]) -> None:
+async def msg_players_sign_in(payload: Dict[str, str]) -> None:
     """
         We have received a player sign-in from the game engine.  We assign that authenticated name
         to the session.  Use for tracking during softboots.
     """
-    player = msg["payload"]["name"]
-    session = msg["payload"]["uuid"]
+    player = payload["name"]
+    session = payload["uuid"]
     if session in clients.connections:
         log.debug(f"players/sign-in received for {player}@{session}")
         clients.connections[session].name = player
 
 
-async def msg_players_sign_out(msg: Dict[str, Dict[str, str]]) -> None:
+async def msg_players_sign_out(payload: Dict[str, str]) -> None:
     """
         We have received a player sign-out message from the engine.  This indicates a player has quit
         the game.  We change player session state to disconnected to end their session.
     """
-    player = msg["payload"]["name"]
-    message = msg["payload"]["message"]
-    session = msg["payload"]["uuid"]
+    player = payload["name"]
+    message = payload["message"]
+    session = payload["uuid"]
     if session in clients.connections:
-        log.debug(f'{msg["event"]} received for {player}@{session}')
+        log.debug(f"players/sign-out received for {player}@{session}")
         clients.connections[session].state["connected"] = False
         asyncio.create_task(messages_to_clients[session].put(Message("IO", message)))
 
 
-async def msg_player_session_command(msg: Dict[str, Dict[str, str]]) -> None:
+async def msg_player_session_command(payload: Dict[str, str]) -> None:
     """
         Any non standard I/O for a player session.
 
@@ -89,8 +88,8 @@ async def msg_player_session_command(msg: Dict[str, Dict[str, str]]) -> None:
         or shouldn't echo locally at specific times (password entry).  Will expand in the future for
         SSH commands and Web Client.
     """
-    session = msg["payload"]["uuid"]
-    command = msg["payload"]["command"]
+    session = payload["uuid"]
+    command = payload["command"]
     if session in clients.connections:
         if clients.connections[session].conn_type == "telnet":
             if command == "do echo":
@@ -102,8 +101,8 @@ async def msg_player_session_command(msg: Dict[str, Dict[str, str]]) -> None:
             asyncio.create_task(messages_to_clients[session].put(Message("COMMAND-TELNET", "", message)))
 
 
-async def msg_game_softboot(msg: Dict[str, Dict[str, str]]) -> None:
-    await softboot_game(int(msg["payload"]["wait_time"]))
+async def msg_game_softboot(payload: Dict[str, str]) -> None:
+    await softboot_game(int(payload["wait_time"]))
 
 
 messages: Dict[str, Callable] = {
@@ -125,4 +124,7 @@ async def message_parse(inp: Union[str, bytes]):
         return
 
     if msg["event"] in messages:
-        asyncio.create_task(messages[str(msg["event"])](msg))
+        if payload := msg.get("payload"):
+            asyncio.create_task(messages[str(msg["event"])](payload))
+        else:
+            asyncio.create_task(messages[str(msg["event"])]())
